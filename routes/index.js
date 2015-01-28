d889e106cd024f1b1777a6f183664ae629b88f44
Koki Takahashi <hakatasiloving@gmail.com>
2015-01-28 10:57:02 +0900

    Remove caption from non-home pages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', {title: 'Home'});
+    res.render('index', {home: true, title: 'Home'});
 });
 
 module.exports = router;
